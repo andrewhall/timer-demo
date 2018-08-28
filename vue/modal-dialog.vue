8dee3d68b7e98e6ca8533ab07284931234688fd3
Andrew Hall <andrew.s.hall.ctr@navy.mil>
2018-08-28 08:03:27 -0400

    Switch to use http vue loader to load vue components directly

---
+++ vue/modal-dialog.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Single-file JavaScript component</h1>
+        <p>{{ message }}</p>
+    </div>
+</template>      
+
+<script>
+    module.exports = {
+        data: function() {
+            return {
+                message: 'Text coming from the component.'
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
