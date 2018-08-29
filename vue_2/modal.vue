75421aff5433d21833ed45a0938fdf6536ca8ef6
Andrew Hall <andrew.s.hall.ctr@navy.mil>
2018-08-29 10:36:47 -0400

    Added different version for test

---
+++ vue_2/modal.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        {{ warningMessage }}
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        data () {
+            return {
+                warningMessage: 'Your session is about to expire. Please close this dialog to prevent timing out.',
+                timedoutMessage: 'Session has timed out. Please close the window and re-access your application via CONOPS.'
+            }
+        },
+        methods: {
+            close() {
+                this.$emit('close');
+            }
+        }
+    }
+</script>
+
+<style></style>
